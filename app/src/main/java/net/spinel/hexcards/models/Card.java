0f8b27d22b96a3a2a6c529186d2db16875d1158b
Spinel <2459075@qq.com>
2016-05-13 18:20:24 +0800

    add db modify assets

--- app/src/main/java/net/spinel/hexcards/models/Card.java
+++ app/src/main/java/net/spinel/hexcards/models/Card.java
@@ -4,59 +4,46 @@ package net.spinel.hexcards.models;
  * Created by Spinel on 16/5/12.
  */
 public class Card {
-    private String ability;
-    private String camp;
     private String card_no;
     private String color;
-    private int cost;
-    private int defense;
-    private String desc;
-    private boolean is_unique;
     private String name;
     private String name_en;
-    private String rarity;
+    private int cost;
     private String requirement;
-    private String subtype;
-    private String type;
     private int power;
+    private int defense;
+    private boolean is_unique;
+    private String type;
+    private String subtype;
+    private String rarity;
+    private String camp;
+    private String ability;
+    private String description;
+    private String img_url;
 
     public Card() {
     }
 
-    public Card(String ability, String camp, String card_no, String color, int cost,
-                int defense, String desc, boolean is_unique, String name, String name_en,
-                String rarity, String requirement, String subtype, String type, int power) {
-        this.ability = ability;
-        this.camp = camp;
+    public Card(String card_no, String color, String name, String name_en, int cost,
+                String requirement, int power, int defense, boolean is_unique, String type,
+                String subtype, String rarity, String camp, String ability, String description,
+                String img_url) {
         this.card_no = card_no;
         this.color = color;
-        this.cost = cost;
-        this.defense = defense;
-        this.desc = desc;
-        this.is_unique = is_unique;
         this.name = name;
         this.name_en = name_en;
-        this.rarity = rarity;
+        this.cost = cost;
         this.requirement = requirement;
-        this.subtype = subtype;
-        this.type = type;
         this.power = power;
-    }
-
-    public String getAbility() {
-        return ability;
-    }
-
-    public void setAbility(String ability) {
-        this.ability = ability;
-    }
-
-    public String getCamp() {
-        return camp;
-    }
-
-    public void setCamp(String camp) {
+        this.defense = defense;
+        this.is_unique = is_unique;
+        this.type = type;
+        this.subtype = subtype;
+        this.rarity = rarity;
         this.camp = camp;
+        this.ability = ability;
+        this.description = description;
+        this.img_url = img_url;
     }
 
     public String getCard_no() {
@@ -75,38 +62,6 @@ public class Card {
         this.color = color;
     }
 
-    public int getCost() {
-        return cost;
-    }
-
-    public void setCost(int cost) {
-        this.cost = cost;
-    }
-
-    public int getDefense() {
-        return defense;
-    }
-
-    public void setDefense(int defense) {
-        this.defense = defense;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public boolean is_unique() {
-        return is_unique;
-    }
-
-    public void setIs_unique(boolean is_unique) {
-        this.is_unique = is_unique;
-    }
-
     public String getName() {
         return name;
     }
@@ -123,12 +78,12 @@ public class Card {
         this.name_en = name_en;
     }
 
-    public String getRarity() {
-        return rarity;
+    public int getCost() {
+        return cost;
     }
 
-    public void setRarity(String rarity) {
-        this.rarity = rarity;
+    public void setCost(int cost) {
+        this.cost = cost;
     }
 
     public String getRequirement() {
@@ -139,12 +94,28 @@ public class Card {
         this.requirement = requirement;
     }
 
-    public String getSubtype() {
-        return subtype;
+    public int getPower() {
+        return power;
     }
 
-    public void setSubtype(String subtype) {
-        this.subtype = subtype;
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public boolean is_unique() {
+        return is_unique;
+    }
+
+    public void setIs_unique(boolean is_unique) {
+        this.is_unique = is_unique;
     }
 
     public String getType() {
@@ -155,32 +126,73 @@ public class Card {
         this.type = type;
     }
 
-    public int getPower() {
-        return power;
+    public String getSubtype() {
+        return subtype;
     }
 
-    public void setPower(int power) {
-        this.power = power;
+    public void setSubtype(String subtype) {
+        this.subtype = subtype;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(String rarity) {
+        this.rarity = rarity;
+    }
+
+    public String getCamp() {
+        return camp;
+    }
+
+    public void setCamp(String camp) {
+        this.camp = camp;
+    }
+
+    public String getAbility() {
+        return ability;
+    }
+
+    public void setAbility(String ability) {
+        this.ability = ability;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImg_url() {
+        return img_url;
+    }
+
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
     }
 
     @Override
     public String toString() {
         return "Card{" +
-                "ability='" + ability + '\'' +
-                ", camp='" + camp + '\'' +
-                ", card_no='" + card_no + '\'' +
+                "card_no='" + card_no + '\'' +
                 ", color='" + color + '\'' +
-                ", cost=" + cost +
-                ", defense=" + defense +
-                ", desc='" + desc + '\'' +
-                ", is_unique=" + is_unique +
                 ", name='" + name + '\'' +
                 ", name_en='" + name_en + '\'' +
-                ", rarity='" + rarity + '\'' +
+                ", cost=" + cost +
                 ", requirement='" + requirement + '\'' +
-                ", subtype='" + subtype + '\'' +
-                ", type='" + type + '\'' +
                 ", power=" + power +
+                ", defense=" + defense +
+                ", is_unique=" + is_unique +
+                ", type='" + type + '\'' +
+                ", subtype='" + subtype + '\'' +
+                ", rarity='" + rarity + '\'' +
+                ", camp='" + camp + '\'' +
+                ", ability='" + ability + '\'' +
+                ", description='" + description + '\'' +
+                ", img_url='" + img_url + '\'' +
                 '}';
     }
 }
